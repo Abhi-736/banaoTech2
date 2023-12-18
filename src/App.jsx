51af03de01bf5dd7416c8c882a4fade5c51cd376
Harry <abhishekchauhan664@gmail.com>
2023-12-18 23:40:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div style={{'font':'30px','height':'100vh','backgroundColor':'red'}} onScroll={(e)=>{console.log(e)}}>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, laboriosam. Expedita perferendis veniam porro minus tempora fugit illo velit magnam voluptatem est officia, laborum nesciunt soluta numquam quidem neque necessitatibus!
+      </div>
+     
+    </>
+  )
+}
+
+export default App
